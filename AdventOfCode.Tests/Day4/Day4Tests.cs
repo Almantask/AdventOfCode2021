f240dd640c8795471956275deb11180da704898c
AlmantasK <almantusk@gmail.com>
2021-12-05 16:23:22 +0200

    Day4 prep

---
+++ AdventOfCode.Tests/Day4/Day4Tests.cs
@@ -0,0 +1,45 @@
+﻿using AdventOfCode.Day3;
+
+namespace AdventOfCode.Tests.Day4
+{
+    public class Day4Tests
+    {
+        [Theory]
+        [MemberData(nameof(Part1))]
+        public void Part1_Solve_Returns(string measures, long expected)
+        {
+            var part1 = new Part1();
+
+            var destination = part1.Solve(measures);
+
+            destination.Should().Be(expected);
+        }
+
+        [Theory]
+        [MemberData(nameof(Part2))]
+        public void Part2_Solve_Returns(string input, long expected)
+        {
+            var part2 = new Part2();
+
+            var destination = part2.Solve(input);
+
+            destination.Should().Be(expected);
+        }
+
+        public static IEnumerable<object[]> Part1
+        {
+            get
+            {
+                yield return Expect(day: 3, file: "Example", result: 0);
+            }
+        }
+
+        public static IEnumerable<object[]> Part2
+        {
+            get
+            {
+                yield return Expect(day: 3, file: "Example", result: 0);
+            }
+        }
+    }
+}
---
+++ AdventOfCode/Day4/Solution.cs
@@ -0,0 +1,25 @@
+﻿using AdventOfCode.Common;
+
+namespace AdventOfCode.Day4
+{
+    public class Solution : AdventOfCodeDay<Part1, Part2>
+    {
+        protected override int Day => 4;
+    }
+
+    public class Part1 : IPartSolution
+    {
+        public long Solve(string input)
+        {
+            return 0;
+        }
+    }
+
+    public class Part2 : IPartSolution
+    {
+        public long Solve(string input)
+        {
+            return 0;
+        }
+    }
+}
