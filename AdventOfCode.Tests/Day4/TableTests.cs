bd4040e7725062b5c163c08487fd32af4d80e275
AlmantasK <almantusk@gmail.com>
2021-12-05 22:04:36 +0200

    Cleanup

--- AdventOfCode.Tests/Day4/TableTests.cs
+++ AdventOfCode.Tests/Day4/TableTests.cs
@@ -1,9 +1,4 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-using AdventOfCode.Day4;
+﻿using AdventOfCode.Day4;
 
 namespace AdventOfCode.Tests.Day4
 {
--- AdventOfCode/Day4/Bingo.cs
+++ AdventOfCode/Day4/Bingo.cs
@@ -51,35 +51,3 @@ public class Bingo
         return false;
     }
 }
-
-public class RiggedBingo : Bingo
-{
-    public RiggedBingo(Table[] tables, byte[] numbersToDraw) : base(tables, numbersToDraw)
-    {
-    }
-
-    public override long? GetWinnersScore()
-    {
-        var lastWinner = Tables.MaxBy(t => t.DrawnNumbersCount);
-        var lasWinnerScore = lastWinner.CalculateScore();
-
-        return lasWinnerScore;
-    }
-
-    public override void Play()
-    {
-        foreach (var numberToDraw in NumbersToDraw)
-        {
-            Draw(numberToDraw);
-            FlagWinners();
-        }
-    }
-
-    private void FlagWinners()
-    {
-        foreach (var table in Tables)
-        {
-            table.IsWinner();
-        }
-    }
-}
