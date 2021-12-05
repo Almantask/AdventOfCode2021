e8bba22e34ae2e4e773ade1c5b732e02a7f581ea
AlmantasK <almantusk@gmail.com>
2021-12-05 22:12:08 +0200

    Day5 prep

---
+++ AdventOfCode.Tests/Day5/Day4Tests.cs
@@ -0,0 +1,45 @@
+﻿using AdventOfCode.Day5;
+
+namespace AdventOfCode.Tests.Day5
+{
+    public class Day5Tests
+    {
+        [Theory]
+        [MemberData(nameof(Part1))]
+        public void Part1_Solve_Returns(string measures, long expected)
+        {
+            var part1 = new Part1();
+
+            var destination = part1.Solve(measures);
+
+            destination.Should().Be(expected);
+        }
+
+        [Theory]
+        [MemberData(nameof(Part2))]
+        public void Part2_Solve_Returns(string input, long expected)
+        {
+            var part2 = new Part2();
+
+            var destination = part2.Solve(input);
+
+            destination.Should().Be(expected);
+        }
+
+        public static IEnumerable<object[]> Part1
+        {
+            get
+            {
+                yield return Expect(day: 5, file: "Example", result: 5);
+            }
+        }
+
+        public static IEnumerable<object[]> Part2
+        {
+            get
+            {
+                yield return Expect(day: 5, file: "Example", result: 0);
+            }
+        }
+    }
+}
---
+++ AdventOfCode/Day5/Solution.cs
@@ -0,0 +1,31 @@
+﻿using AdventOfCode.Common;
+using AdventOfCode.Day4;
+
+namespace AdventOfCode.Day5
+{
+    public class Solution : AdventOfCodeDay<Part1, Part2>
+    {
+        protected override int Day => 5;
+    }
+
+    public class Part1 : IPartSolution
+    {
+        public long Solve(string input)
+        {
+            return 0;
+        }
+    }
+
+    public class Part2 : IPartSolution
+    {
+        public long Solve(string input)
+        {
+            return 0;
+        }
+    }
+
+    internal static class PartInputTransformer
+    {
+
+    }
+}
